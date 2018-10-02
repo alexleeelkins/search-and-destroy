c44c2a3d994bb017b9c61b0975c26dcdfaa3578d
Alex Elkins <alex.elkins@improving.com>
2018-10-02 15:38:34 -0400

    Basic instance -- does lowercased song title contain search string

---
+++ Search and Destroy/SongModel.swift
@@ -0,0 +1,21 @@
+//
+//  SongModel.swift
+//  Search and Destroy
+//
+//  Created by Alex Elkins on 10/2/18.
+//  Copyright © 2018 Alex Elkins. All rights reserved.
+//
+
+import Foundation
+
+class Song: Searchable {
+    var title: String = ""
+
+    init(title: String) {
+        self.title = title
+    }
+}
+
+protocol Searchable {
+    var title: String { get set }
+}
